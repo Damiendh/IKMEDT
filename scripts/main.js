9f8ad7455fdb2188fda0eb9abea37570fa7c4b60
Damiendh <43526215+Damiendh@users.noreply.github.com>
2019-06-20 17:32:04 +0200

    Add notification sound event to specialization spheres

--- scripts/main.js
+++ scripts/main.js
@@ -8,29 +8,35 @@ window.onload = function(){
   const sphere3 = document.getElementById("sphere3");
   const sphere4 = document.getElementById("sphere4");
 
+  const notificationSound = document.getElementById("notificationSound")
+
   var color = "gray";
 
   sphere1.onmouseenter = function(){
     color = "orange";
     boxScreen.setAttribute("src","#BDAMimg");
     box1.setAttribute("color",color);
+    notificationSound.play("#notificationSound");
   }
 
   sphere2.onmouseenter = function(){
     color = "green";
     boxScreen.setAttribute("src","#IATimg");
     box1.setAttribute("color",color);
+    notificationSound.play("#notificationSound");
   }
 
   sphere3.onmouseenter = function(){
     color = "purple";
     boxScreen.setAttribute("src","#FICTimg");
     box1.setAttribute("color",color);
+    notificationSound.play("#notificationSound");
   }
 
   sphere4.onmouseenter = function(){
     color = "cyan";
     boxScreen.setAttribute("src","#SEimg");
     box1.setAttribute("color",color);
+    notificationSound.play("#notificationSound");
   }
 }
