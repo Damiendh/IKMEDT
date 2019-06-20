330c77f92c2e94ebc90eced9e86bee1a53912a18
Damiendh <43526215+Damiendh@users.noreply.github.com>
2019-06-20 16:55:33 +0200

    Add interaction with specialization spheres

---
+++ scripts/main.js
@@ -0,0 +1,36 @@
+window.onload = function(){
+  const box1 = document.getElementById("box1");
+
+  const boxScreen = document.getElementById("boxScreen");
+
+  const sphere1 = document.getElementById("sphere1");
+  const sphere2 = document.getElementById("sphere2");
+  const sphere3 = document.getElementById("sphere3");
+  const sphere4 = document.getElementById("sphere4");
+
+  var color = "gray";
+
+  sphere1.onmouseenter = function(){
+    color = "orange";
+    boxScreen.setAttribute("src","#BDAMimg");
+    box1.setAttribute("color",color);
+  }
+
+  sphere2.onmouseenter = function(){
+    color = "green";
+    boxScreen.setAttribute("src","#IATimg");
+    box1.setAttribute("color",color);
+  }
+
+  sphere3.onmouseenter = function(){
+    color = "purple";
+    boxScreen.setAttribute("src","#FICTimg");
+    box1.setAttribute("color",color);
+  }
+
+  sphere4.onmouseenter = function(){
+    color = "cyan";
+    boxScreen.setAttribute("src","#SEimg");
+    box1.setAttribute("color",color);
+  }
+}
